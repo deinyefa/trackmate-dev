d9220aff7f3d97262aaee18d97ceb138fde776d1
Deinyefa Eporwei <df.eporwei@gmail.com>
2018-03-27 21:54:38 -0400

    firebase.js

---
+++ src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID
+} from './variables';
+
+var config = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID
+};
+firebase.initializeApp(config);
