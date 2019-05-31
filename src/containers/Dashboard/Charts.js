05ed9872302dc98e60ee89af9bed5fcea99429bb
Deinyefa <df.eporwei@gmail.com>
2019-05-30 20:34:28 -0400

    adding charts to the dashboard

--- src/containers/Dashboard/Charts.js
+++ src/containers/Dashboard/Charts.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
+import { Row, Card, CardBody, CardFooter, CardHeader, Col } from "reactstrap";
 import moment from "moment";
+import Chart from "react-apexcharts";
+import { MdPoll } from "react-icons/md";
 
 class Charts extends Component {
+	componentDidUpdate = () => {
+		this.constructChartData();
+	};
+
 	getOrderStatusHandler = (initialArr = [], monthsArr = [], status = "") =>
 		monthsArr.map(
 			month =>
@@ -10,7 +17,7 @@ class Charts extends Component {
 				).length
 		);
 
-	constructChartData() {
+	constructChartData = () => {
 		const { merchantCustomers } = this.props;
 
 		let updatedMerchantCustomers = merchantCustomers.map(customer => {
@@ -22,7 +29,6 @@ class Charts extends Component {
 				updatedMerchantCustomers[c].updatedOn.toDate()
 			).format("MMM YYYY");
 		}
-		// console.log(updatedMerchantCustomers);
 
 		let months = [];
 		for (let i = 0; i <= 12; i++) {
@@ -33,43 +39,232 @@ class Charts extends Component {
 			);
 		}
 
-		let recievedArr = this.getOrderStatusHandler(
-			updatedMerchantCustomers,
-			months,
-			"recieved"
-		);
-		let processedArr = this.getOrderStatusHandler(
-			updatedMerchantCustomers,
-			months,
-			"processed"
-		);
-		let productionArr = this.getOrderStatusHandler(
-			updatedMerchantCustomers,
-			months,
-			"production"
-		);
-		let shippingArr = this.getOrderStatusHandler(
-			updatedMerchantCustomers,
-			months,
-			"shipping"
-		);
-		let outForDeliveryArr = this.getOrderStatusHandler(
-			updatedMerchantCustomers,
-			months,
-			"outForDelivery"
-		);
-		let deliveredArr = this.getOrderStatusHandler(
-			updatedMerchantCustomers,
-			months,
-			"delivered"
-		);
-		// console.log(months);
-		// console.log(recievedArr);
-	}
+		let lineOptions = {
+			chart: {
+				id: "status-lines",
+				animations: {
+					enabled: true,
+					easing: "bounce", // linear, easeout, easein, easeinout, swing, bounce, elastic
+					speed: 800,
+					animateGradually: {
+						delay: 150,
+						enabled: true,
+					},
+				},
+				// fillSeriesColor: true,
+				fontFamily: "Open Sans, Arial, sans-serif",
+				// stroke: {
+				// 	show: true,
+				// 	curve: "smooth", // "smooth" / "straight" / "stepline"
+				// 	lineCap: "round", // round, butt , square
+				// 	width: 1,
+				// 	dashArray: 0, // single value or array of values
+				// },
+			},
+			xaxis: {
+				categories: months,
+			},
+		};
+
+		let donutSeries = [
+			this.getOrderStatusHandler(
+				updatedMerchantCustomers,
+				months,
+				"recieved"
+			).reduce((sum, el) => {
+				return sum + el;
+			}, 0),
+			this.getOrderStatusHandler(
+				updatedMerchantCustomers,
+				months,
+				"processed"
+			).reduce((sum, el) => {
+				return sum + el;
+			}, 0),
+			this.getOrderStatusHandler(
+				updatedMerchantCustomers,
+				months,
+				"production"
+			).reduce((sum, el) => {
+				return sum + el;
+			}, 0),
+			this.getOrderStatusHandler(
+				updatedMerchantCustomers,
+				months,
+				"shipping"
+			).reduce((sum, el) => {
+				return sum + el;
+			}, 0),
+			this.getOrderStatusHandler(
+				updatedMerchantCustomers,
+				months,
+				"outForDelivery"
+			).reduce((sum, el) => {
+				return sum + el;
+			}, 0),
+			this.getOrderStatusHandler(
+				updatedMerchantCustomers,
+				months,
+				"delivered"
+			).reduce((sum, el) => {
+				return sum + el;
+			}, 0),
+		];
+
+		let lineSeries = [
+			{
+				name: "Recieved",
+				data: this.getOrderStatusHandler(
+					updatedMerchantCustomers,
+					months,
+					"recieved"
+				),
+			},
+			{
+				name: "Processed",
+				data: this.getOrderStatusHandler(
+					updatedMerchantCustomers,
+					months,
+					"processed"
+				),
+			},
+			{
+				name: "Production",
+				data: this.getOrderStatusHandler(
+					updatedMerchantCustomers,
+					months,
+					"production"
+				),
+			},
+			{
+				name: "Shipping",
+				data: this.getOrderStatusHandler(
+					updatedMerchantCustomers,
+					months,
+					"shipping"
+				),
+			},
+			{
+				name: "Out For Delivery",
+				data: this.getOrderStatusHandler(
+					updatedMerchantCustomers,
+					months,
+					"outForDelivery"
+				),
+			},
+			{
+				name: "Delivered",
+				data: this.getOrderStatusHandler(
+					updatedMerchantCustomers,
+					months,
+					"delivered"
+				),
+			},
+		];
+
+		let donutLabels = [
+			"Recieved",
+			"Processed",
+			"Production",
+			"Shipping",
+			"Out For Delivery",
+			"Delivered",
+		];
+
+		let donutOptions = {
+			labels: donutLabels,
+			chart: {
+				animations: {
+					enabled: true,
+					easing: "bounce", // linear, easeout, easein, easeinout, swing, bounce, elastic
+					speed: 800,
+					animateGradually: {
+						delay: 150,
+						enabled: true,
+					},
+				},
+			},
+			plotOptions: {
+				pie: {
+					donut: {
+						size: "60%",
+						labels: {
+							show: true,
+						},
+						total: {
+							show: true,
+						},
+					},
+				},
+			},
+			height: 200,
+		};
+		return [lineOptions, lineSeries, donutSeries, donutOptions];
+	};
 
 	render() {
-		this.constructChartData();
-		return <h2>Charts</h2>;
+		let chartData = this.constructChartData();
+
+		return (
+			<Row>
+				<Col md="12" lg="12">
+					<Card>
+						<CardHeader
+							className="card-chart chart line-graph"
+							data-background-color="green">
+							<h3 className="title">Order Status</h3>
+							<p className="category">
+								All your orders in the last year
+							</p>
+						</CardHeader>
+						<CardBody className="card-content">
+							<Chart
+								options={chartData[0]}
+								series={chartData[1]}
+								type="line"
+								// width="500"
+							/>
+						</CardBody>
+						<CardFooter>
+							<div className="stats">
+								<i className="material-icons">
+									<MdPoll />
+								</i>
+								updated just now
+							</div>
+						</CardFooter>
+					</Card>
+				</Col>
+				<Col md="12" lg="6">
+					<Card>
+						<CardHeader
+							className="card-chart chart line-graph"
+							data-background-color="green">
+							<h3 className="title">Order Status</h3>
+							<p className="category">
+								All your orders in the last year
+							</p>
+						</CardHeader>
+						<CardBody className="card-content">
+							<Chart
+								options={chartData[3]}
+								series={chartData[2]}
+								type="donut"
+								// width="500"
+							/>
+						</CardBody>
+						<CardFooter>
+							<div className="stats">
+								<i className="material-icons">
+									<MdPoll />
+								</i>
+								updated just now
+							</div>
+						</CardFooter>
+					</Card>
+				</Col>
+			</Row>
+		);
 	}
 }
 
